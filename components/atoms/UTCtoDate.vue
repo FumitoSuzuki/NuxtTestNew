49118e70ac813230528d947d6e01a0e7f1ce69f3
FumiSuzuki <42036663+FumitoSuzuki@users.noreply.github.com>
2021-01-08 11:13:51 +0900

    First commit.

---
+++ components/atoms/UTCtoDate.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ updatedAt }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    utc: { type: String, default: '' },
+  },
+  computed: {
+    updatedAt() {
+      const inUTC = new Date(this.utc)
+      return `${inUTC.getFullYear()}/${inUTC.getMonth()}/${inUTC.getDate()}`
+    },
+  },
+}
+</script>
